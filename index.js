d36789942e4afbec34fd4d1ee8cfb9008b9df8f6
mdsajib50 <mdajib850@gmail.com>
2022-10-27 22:00:52 +0600

    data loaded

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const courses =  require('./data/courses.json');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get ('/courses', (req, res)=>{
+
+    res.send(courses)
 })
 
 app.listen(port, () => {
